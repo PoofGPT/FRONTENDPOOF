e96b796f4840766e43a9ea7e6979164a926fcd2e
Boril <77511500+borilliano@users.noreply.github.com>
2025-06-02 23:44:27 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,14 @@
-// pages/_app.js
 import "../styles/globals.css";
-
-// Solana wallet adapter imports
 import { FC, ReactNode, useMemo } from "react";
 import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-
-// Default styles that your app can override
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 const MyApp: FC<{ Component: any; pageProps: any }> = ({ Component, pageProps }) => {
-  // You can set the cluster/network here (mainnet-beta, devnet, testnet)
   const network = WalletAdapterNetwork.Mainnet;
-
-  // You may want to fetch this from your environment (.env)
   const endpoint = process.env.NEXT_PUBLIC_SOLANA_RPC || "https://api.mainnet-beta.solana.com";
-
-  // Configure the wallets you want to support
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
 
   return (
